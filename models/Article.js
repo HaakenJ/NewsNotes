eacc3dff4e25d5bcba864446d3968a8e78475781
Haakasaurus <kramerhjohnson@gmail.com>
2020-01-08 16:22:31 -0800

    Started Note Schema.  Installed eslint but it isn't working...

--- models/Article.js
+++ models/Article.js
@@ -21,7 +21,7 @@ const ArticleSchema = new Schema({
     note: {
         type: Schema.Types.ObjectId,
         ref: "Note",
-    }
+    },
 });
 
 const Article = mongoose.model("Article", ArticleSchema);
